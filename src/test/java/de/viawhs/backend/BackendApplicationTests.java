f91c3ef6f4addd6c9991d8d765ed1ba801fb5205
Nhan Vu <nhan997@gmail.com>
2019-10-21 20:52:35 +0200

    Startup settings

---
+++ src/test/java/de/viawhs/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.viawhs.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
