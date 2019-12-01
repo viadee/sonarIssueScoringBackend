8bb2b7dccebe7db1f78afd59b48ade39774d6d7e
k-backes <kira.backes@studmail.w-hs.de>
2019-12-01 19:11:37 +0100

    update ServerControllerIT

--- src/test/java/de/viawhs/backend/rest/ServerControllerIT.java
+++ src/test/java/de/viawhs/backend/rest/ServerControllerIT.java
@@ -19,22 +19,35 @@ public class ServerControllerIT {
     @Test
     public void contextLoad() throws JSONException {
 
-        /*Beginning Tests: Getting all public Repositories*/
-        String responseFilledArray = this.restTemplate.getForObject(
-                "/api/server/git-repo?username=trnhan251", String.class);
+        /*Beginning Tests: Getting all public repositories*/
+        String responsePublicReposFilled = this.restTemplate.getForObject(
+                "http://localhost:3000/server/git-repo/public?username=trnhan251", String.class);
         JSONAssert.assertEquals(
                 "[{id:191923132}, {id:181204192}, {id:203966717}, {id:215288135}]",
-                responseFilledArray,
+                responsePublicReposFilled,
                 false);
 
-        String responseEmptyArray = this.restTemplate.getForObject(
-                "/api/server/git-repo?username=k-backes", String.class);
+        String responsePublicReposEmpty = this.restTemplate.getForObject(
+                "http://localhost:3000/server/git-repo/public?username=k-backes", String.class);
         JSONAssert.assertEquals(
-                "[]",
-                responseEmptyArray,
-                false);
-        /*End Tests: Getting all public Repositories*/
+                "[]", responsePublicReposEmpty, false);
 
+        String responsePublicWrongUser = this.restTemplate.getForObject(
+                "http://localhost:3000/server/git-repo/public?username=khfslhdfhsdfhdsfhdfjdfjdjfhsdfk", String.class);
+        JSONAssert.assertEquals("{status:500}", responsePublicWrongUser, false);
+        /*End Tests: Getting all public repositories*/
+
+
+        /*Beginning Tests: Getting all branches of a public repository*/
+        String responseBranchesPublic = this.restTemplate.getForObject(
+                "http://localhost:3000/server/git-repo/public/branches?username=trnhan251&repo=sonarIssueScoringBackend",
+                String.class);
+        JSONAssert.assertEquals("[{name:master}]", responseBranchesPublic, false);
+
+        String responseWrongBranchPublic = this.restTemplate.getForObject(
+                "http://localhost:3000/server/git-repo/public/branches?username=trnhan251&repo=wrongbranchname", String.class);
+        JSONAssert.assertEquals("{status:500}", responseWrongBranchPublic, false);
+        /*End Tests: Getting all branches of a public repository*/
 
     }
 
