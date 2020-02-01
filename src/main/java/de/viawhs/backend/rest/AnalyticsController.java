1b11917e3732b71fe74acaab5eb5a663afea81d7
k-backes <kira.backes@studmail.w-hs.de>
2020-02-01 16:53:55 +0100

    merging

--- src/main/java/de/viawhs/backend/rest/AnalyticsController.java
+++ src/main/java/de/viawhs/backend/rest/AnalyticsController.java
@@ -1,26 +1,45 @@
 package de.viawhs.backend.rest;
 
+import de.viawhs.backend.model.ResultFile;
 import de.viawhs.backend.model.Wizard;
 import de.viawhs.backend.service.AnalyticsService;
+import de.viawhs.backend.service.FileService;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/server/analytics")
 public class AnalyticsController {
     private AnalyticsService analyticsService;
-
-    public AnalyticsController(AnalyticsService analyticsService) {
+    private FileService fileService;
+    private Date date;
+    public AnalyticsController(AnalyticsService analyticsService, FileService fileService) {
         this.analyticsService = analyticsService;
+        this.fileService = fileService;
     }
 
     @PostMapping("/change-count")
     public String predictChangeCount(@RequestBody Wizard wizard) {
-        return this.analyticsService.predictChangeCount(wizard);
+        //String result = this.analyticsService.predictChangeCount(wizard);
+        String result = "Test";
+        date = Calendar.getInstance().getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh:mm:ss");
+        ResultFile resultFile = new ResultFile(wizard.getUser(), dateFormat.format(date), wizard.getUrl().split("/")[wizard.getUrl().split("/").length-1], result);
+        fileService.saveResultFile(resultFile);
+        return result;
     }
 
     @PostMapping("/ordering-issues")
     public String orderingIssues(@RequestBody Wizard wizard) {
-        return this.analyticsService.orderingIssues(wizard);
+        String result = this.analyticsService.orderingIssues(wizard);
+        date = Calendar.getInstance().getTime();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh:mm:ss");
+        ResultFile resultFile = new ResultFile(wizard.getUser(), dateFormat.format(date), wizard.getUrl().split("/")[wizard.getUrl().split("/").length-1], result);
+        fileService.saveResultFile(resultFile);
+        return result;
     }
 
 }
