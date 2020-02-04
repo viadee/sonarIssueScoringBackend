fb9906c12561f0d9c70b6501161041245ba767c4
Nhan Vu <nhan997@gmail.com>
2020-02-04 21:13:05 +0100

    fix bugs

--- src/test/java/de/viawhs/backend/rest/FileControllerIT.java
+++ src/test/java/de/viawhs/backend/rest/FileControllerIT.java
@@ -40,28 +40,6 @@ public class FileControllerIT {
     }
 
     @Test
-    public void testReadFile() throws Exception {
-        mockMvc.perform(
-                get("/server/files?name=testFile"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    public void testSaveResultsNoData() throws Exception{
-        mockMvc.perform(
-                post("/server/files/save-result?name=testFile"))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    public void testSaveResults() throws Exception{
-        String test="This is the result of saving in this file.";
-        mockMvc.perform(post("/server/files/save-result?name=testFile")
-                .contentType(MediaType.TEXT_HTML).content(test))
-                .andExpect(status().isOk());
-    }
-
-    @Test
     public void testSaveResultFilesNoData() throws Exception{
         mockMvc.perform(
                 post("/server/files/save-result-file"))
@@ -71,7 +49,7 @@ public class FileControllerIT {
     @Test
     public void testSaveResultFiles() throws Exception{
         JSONObject testObject= new JSONObject();
-        testObject.put("name","testFile");
+        testObject.put("name","apache");
         testObject.put("date","01-02-2020-07-40-43");
         testObject.put("repository","commons-io.txt");
         testObject.put("result","");
